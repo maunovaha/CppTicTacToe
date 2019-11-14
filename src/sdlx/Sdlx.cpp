ea6cc61a7ac359b385532ddd2478b90e28cdfabe
Mauno Vähä <maunovaha@gmail.com>
2019-11-14 15:47:52 +0200

    Modifies Sdlx to init SDL_image in order to load images

--- src/sdlx/Sdlx.cpp
+++ src/sdlx/Sdlx.cpp
@@ -1,4 +1,5 @@
 #include "Sdlx.h"
+#include <SDL2/SDL_image.h>
 #include <stdexcept>
 #include <string>
 
@@ -7,12 +8,19 @@ namespace sdlx {
 Sdlx::Sdlx(const Uint32 flags)
 {
     if (SDL_Init(flags) != 0) {
-        throw std::runtime_error("Could not initialize SDL, " + std::string{SDL_GetError()});
+        throw std::runtime_error("Could not init SDL, " + std::string{SDL_GetError()});
+    }
+
+    int img_flags = IMG_INIT_PNG;
+
+    if (!(IMG_Init(img_flags) & img_flags)) {
+        throw std::runtime_error("Could not init SDL_image, " + std::string{IMG_GetError()});
     }
 }
 
 Sdlx::~Sdlx()
 {
+    IMG_Quit();
     SDL_Quit();
 }
 
