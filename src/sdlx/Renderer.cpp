26b343338dcb8ef0dfbb68c3ee33346a1f217349
Mauno Vähä <maunovaha@gmail.com>
2019-11-28 13:44:25 +0200

    Changes sdlx::Renderer to use smart pointers

--- src/sdlx/Renderer.cpp
+++ src/sdlx/Renderer.cpp
@@ -6,23 +6,16 @@ namespace sdlx {
 
 Renderer::Renderer(const Window& window, const SDL_Color& clear_color, const Uint32 flags) 
     : clear_color_{clear_color}
+    , renderer_{SDL_CreateRenderer(window.get(), -1, flags), &SDL_DestroyRenderer}
 {
-    renderer_ = SDL_CreateRenderer(window.get(), -1, flags);
-
     if (!renderer_) {
         throw std::runtime_error("Could not create renderer, " + std::string{SDL_GetError()});
     }
 }
 
-Renderer::~Renderer()
-{
-    SDL_DestroyRenderer(renderer_);
-    renderer_ = nullptr;
-}
-
 void Renderer::set_draw_color(const SDL_Color& color) const
 {
-    if (SDL_SetRenderDrawColor(renderer_, color.r, color.g, color.b, color.a) != 0) {
+    if (SDL_SetRenderDrawColor(renderer_.get(), color.r, color.g, color.b, color.a) != 0) {
         throw std::runtime_error("Could not set renderer draw color, " + std::string{SDL_GetError()});
     }
 }
@@ -31,19 +24,19 @@ void Renderer::clear() const
 {
     set_draw_color(clear_color_);
 
-    if (SDL_RenderClear(renderer_) != 0) {
+    if (SDL_RenderClear(renderer_.get()) != 0) {
         throw std::runtime_error("Could not clear renderer, " + std::string{SDL_GetError()});
     }
 }
 
 void Renderer::present() const
 {
-    SDL_RenderPresent(renderer_);
+    SDL_RenderPresent(renderer_.get());
 }
 
 SDL_Renderer* Renderer::get() const
 {
-    return renderer_;
+    return renderer_.get();
 }
 
 }
--- src/sdlx/Renderer.h
+++ src/sdlx/Renderer.h
@@ -3,20 +3,20 @@
 
 #include "Window.h"
 #include <SDL2/SDL.h>
+#include <memory>
 
 namespace sdlx {
 
 class Renderer {
 public:
     Renderer(const Window& window, const SDL_Color& clear_color, const Uint32 flags);
-    ~Renderer();
     void set_draw_color(const SDL_Color& color) const;
     void clear() const;
     void present() const;
     SDL_Renderer* get() const;
 private:
     SDL_Color clear_color_;
-    SDL_Renderer* renderer_ = nullptr;
+    std::unique_ptr<SDL_Renderer, decltype(&SDL_DestroyRenderer)> renderer_;
 };
 
 }
