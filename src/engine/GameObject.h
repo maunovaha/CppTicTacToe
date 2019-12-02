e6a409475ea645d38a44509f810816621af25955
Mauno Vähä <maunovaha@gmail.com>
2019-12-02 17:20:42 +0200

    Changes update() and render() to be pure virtual at GameObject

--- src/engine/GameObject.h
+++ src/engine/GameObject.h
@@ -8,8 +8,8 @@ public:
     GameObject() = default;
     GameObject(const int x, const int y) : x_{x}, y_{y} {}
     virtual ~GameObject() = default;
-    virtual void update() {}
-    virtual void render() const {}
+    virtual void update() = 0;
+    virtual void render() const = 0;
 protected:
     int x_ = 0;
     int y_ = 0;
