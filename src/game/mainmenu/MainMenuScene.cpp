a392fec1c907b314c9d3021b39a9c22cc7ff179d
Mauno Vähä <maunovaha@gmail.com>
2019-12-09 17:42:36 +0200

    Adds setPosition() to GameObject

--- src/game/mainmenu/MainMenuScene.cpp
+++ src/game/mainmenu/MainMenuScene.cpp
@@ -12,16 +12,20 @@ void MainMenuScene::create(const engine::Window& window, engine::TextureCache& t
         {
             {"background", std::make_shared<engine::SpriteClip>(0, 0, 360, 360)},
             {"x",          std::make_shared<engine::SpriteClip>(360, 0, 120, 120)},
-            {"o",          std::make_shared<engine::SpriteClip>(360, 360, 120, 120)}
+            {"o",          std::make_shared<engine::SpriteClip>(360, 120, 120, 120)}
         }
     };
 
     const std::shared_ptr<engine::Sprite> background = spriteSheet.getSprite("background");
-    const std::shared_ptr<engine::Sprite> x = spriteSheet.getSprite("x");
-    const std::shared_ptr<engine::Sprite> o = spriteSheet.getSprite("o");
-
+    background->setPosition(50, 50);
     addChild(background);
+
+    const std::shared_ptr<engine::Sprite> x = spriteSheet.getSprite("x");
+    x->setPosition(50, 50);
     addChild(x);
+
+    const std::shared_ptr<engine::Sprite> o = spriteSheet.getSprite("o");
+    o->setPosition(170, 170);
     addChild(o);
 }
 
