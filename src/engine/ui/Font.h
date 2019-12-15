fc0925da1a0277ad06da330f9928364449d6bc07
Mauno Vähä <maunovaha@gmail.com>
2019-12-15 18:04:37 +0200

    Implements new ui::Font class

--- src/engine/ui/Font.h
+++ src/engine/ui/Font.h
@@ -1,15 +1,35 @@
 #pragma once
 
-#include "../../sdl2wrap/Font.h"
+#include "Color.h"
+#include "../gfx/Surface.h"
+#if defined(_WIN32)
+    #include <SDL.h>
+    #include <SDL_ttf.h>
+#else
+    #include <SDL2/SDL.h>
+    #include <SDL2/SDL_ttf.h>
+#endif
+#include <string>
+#include <memory>
 
 namespace engine::ui {
 
 class Font {
 public:
-    Font(const std::string& filePath, const int size) : font_{filePath, size} {}
-    sdl2wrap::Surface renderBlended(const std::string& text, const SDL_Color& color) const;
+    Font(const std::string& filePath, const int size);
+    gfx::Surface renderBlended(const std::string& text, const Color& color) const;
+    TTF_Font* get() const;
 private:
-    sdl2wrap::Font font_;
+    struct DestroyFont {
+        void operator()(TTF_Font* font) const
+        {
+            TTF_CloseFont(font);
+        }
+    };
+
+    using UniqueFont = std::unique_ptr<TTF_Font, DestroyFont>;
+
+    UniqueFont font_;
 };
 
 }
